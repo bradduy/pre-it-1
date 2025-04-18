8f818f57204d8724ef309d0b50da86b8ed1a3f33
Brad Duy <tranthanhduy279@gmail.com>
2025-04-19 02:10:58 +0900

    add practice for class

---
+++ practice_class.py
@@ -0,0 +1,14 @@
+user_data = {
+   "id": 20250418,
+   "name": "Jennie",
+   "email": "jennie@gmail.com",
+   "address": {
+       "street": "123 Main St",
+       "city": "Seoul",
+       "zipcode": "12345"
+   },
+   "is_active": True
+}
+
+profile = UserProfile(user_data)
+# TO-DO:updated_user_data = ………
